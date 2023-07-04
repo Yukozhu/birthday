210d273dc51496fc3645e004d6dc7b680546539d
Yukozhu <138542118+Yukozhu@users.noreply.github.com>
2023-07-03 20:16:55 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,20 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "Happy BIRTH",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "15 GO DRIVE",  // 同上...
+        "MAKING THIS IS",
+        "SO FUN",
+        "UR AP IS",
+        "GONNABEFINE",
+        "I CANT TYPE",
+        "MORE THAN 15",
+        "AT A TIME",
+        "R U SURE U DONT",
+        "WANT TO GO TO",
+        "DISNEY",
+        "LMFAO JK IGETIT",
+        "HAVE SM FUN",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,18 +28,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "HAHAHHAHHAHA": "./imgs/IMG_8754.jpg",
+        "今天是你的生日": "./imgs/IMG_9219.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Colors",
+        balloons_flying: "Something Missing",
+        cake_fadein: "Cake？",
+        light_candle: "Candles？",
+        wish_message: "Hbd",
+        story: "ClickMe",
     }
 };
