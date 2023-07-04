0969e2e5f83b3f9c1f913dcdaabcf71e675bf8c0
Yukozhu <138542118+Yukozhu@users.noreply.github.com>
2023-07-04 11:40:32 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,10 @@ var config = {
     texts: [
         "Happy BIRTH",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "15 GO DRIVE",  // 同上...
-        "MAKING THIS IS",
-        "SO FUN",
+        "WE NEED TO TAKE",
+        "PHOTOS TOGETHER",
+        "IN JAPAN",
+        "I DONTHAVE MUCH",
         "UR AP IS",
         "GONNABEFINE",
         "I CANT TYPE",
@@ -17,7 +19,7 @@ var config = {
         "U LIVED THROUGH",
         "5475 DAYS ALR",
         "DONT DIE",
-        "BYE",
+        "BYEBYE",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -31,6 +33,16 @@ var config = {
     imgs: {
         "Happy BIRTH": "./imgs/IMG_8754.jpg",
         "15 GO DRIVE": "./imgs/IMG_9219.jpg",
+        "WE NEED TO TAKE": "./imgs/IMG_8793.jpg",
+        "PHOTOS TOGETHER",: "./imgs/IMG_8598.jpg",
+        "IN JAPAN",: "./imgs/IMG_8597.jpg",
+        "I DONTHAVE MUCH",: "./imgs/IMG_8595.jpg",
+        "UR AP IS",: "./imgs/IMG_7946.jpg",
+        "GONNABEFINE",: "./imgs/IMG_7382.jpg",
+        "HAPPY U DAY",: "./imgs/IMG_8549.jpg",
+        "U LIVED THROUGH",: "./imgs/IMG_8093.jpg",
+        "5475 DAYS ALR",: "./imgs/IMG_7707.JPG",
+        "DONT DIE",: "./imgs/IMG_7516.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
@@ -40,7 +52,7 @@ var config = {
         balloons_flying: "Something Missing",
         cake_fadein: "Cake？",
         light_candle: "Candles？",
-        wish_message: "Hbd",
+        wish_message: "Weeeeee",
         story: "ClickMe",
     }
 };
